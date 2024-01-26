93a3b8c163a130436f94770a3428446ecd9ea7d1
m-sarful <isarful823@gmail.com>
2024-01-26 17:11:50 +0600

    added task2 of mosule 16

---
+++ task2_M17.js
@@ -0,0 +1,11 @@
+var mathematics = 75.25;
+ var biology = 99.50;
+ var chemistry = 65;
+ var physics = 80;
+ var bangla = 35.45;
+ var totalSub = 5;
+  var totalNum = mathematics + biology + chemistry + physics + bangla;
+   console.log(totalNum);
+   var avg = totalNum / totalSub;
+    console.log(avg);
+ 
